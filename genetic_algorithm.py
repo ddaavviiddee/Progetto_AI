0d678f1c0dd032154c2d06349555036541edba01
ddaavviiddee <davidemento02@gmail.com>
2024-02-08 23:47:10 +0100

    Final final commit

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,19 +1,21 @@
 import random
 import numpy as np
-from neural_network import create_cnn, train_cnn
-from copy import deepcopy
+from neural_network import *
 import csv
 import time
+import math
 
 
-# Funzione di fitness
-def fitness(individual):
-    # Addestra la CNN con i parametri dell'individuo e restituisci l'accuracy
-    print("Training with individual")
+# Funzione per l'addestramento
+def fitness(individual, epochs):
+    # Addestra la CNN con i parametri dell'individuo 
     num_conv_layers, conv_size, num_fc_layers, fc_size = individual
     cnn_model = create_cnn(num_conv_layers, conv_size, num_fc_layers, fc_size)
-    accuracy = train_cnn(cnn_model, epochs=10)
-    return accuracy
+    start_time = time.time()
+    accuracy, history = train_cnn(cnn_model, epochs)
+    training_time = time.time() - start_time;
+    fitness_score = accuracy / math.log10(training_time)  # La fitness è data dall'accuracy fratto il log10 del tempo di addestramento
+    return accuracy, fitness_score, training_time, history, individual
 
 # Inizializzazione della popolazione
 def initialize_population(population_size):
@@ -43,64 +45,73 @@ def crossover(parent1, parent2):
     return child1, child2
 
 # Mutazione
-def mutate(individual, mutation_rate=0.1):
-    mutated_individual = deepcopy(individual) # Deepcopy assicura che sia una copia separata dall'originale 
+def mutate(mutated_individual, mutation_rate=0.1):
     for i in range(len(mutated_individual)): # Cicla gene per gene
         if random.random() < mutation_rate: # Mutation rate del 10%
             mutated_individual[i] = random.randint(1, 5) if i % 2 == 0 else random.randint(16, 256) # Mutazione dell'individuo
-            # Se l'indice è pari allora il gene corrisponde al numero di strati convoluzionali
+            # Se l'indice è pari allora il gene corrisponde al numero di strati convoluzionali o fully-connected
             # Altrimenti corrisponde alla dimensione dello strato
     return mutated_individual
 
-# Funzione per salvare la popolazione in un file CSV
-def save_individuals(filename, best_individual, best_accuracy):
-    header = ["NumConvLayers", "ConvSize", "NumFCLayers", "FCSize", "Accuracy"]
-    with open(filename, mode='a', newline='') as file:  # Usa 'a' per appendere al file
+def save_individual(filename, generation, individual, accuracy, fitness_score, training_time):
+    header = ["Generation", "NumConvLayers", "ConvSize", "NumFCLayers", "FCSize", "Accuracy", "Fitness", "Training time"] # Intestazione
+    data_to_write = [generation] + individual + [accuracy, fitness_score, training_time] # Si assicura che sia una lista
+    with open(filename, mode='a', newline='') as file:
         writer = csv.writer(file)
-        if file.tell() == 0:  # Scrivi l'header solo se il file è vuoto
+        if file.tell() == 0: # Se il file è vuoto allora scrivi l'intestazione
             writer.writerow(header)
-        # Scrivi i dati del miglior individuo di questa generazione
-        data_to_write = list(best_individual) + [best_accuracy]
-        writer.writerow(map(str, data_to_write))
+        writer.writerow(map(str, data_to_write)) # Converte ogni elemento in una stringa
 
-        
-
-# Esempio di utilizzo dell'algoritmo genetico
 population_size = 20
-generations = 10
+target_fitness = 0.33  # Fitness desiderata
 population = initialize_population(population_size)
+epochs = 10
 
-# Esempio di utilizzo delle funzioni di salvataggio e caricamento
 population_filename = 'population.csv'
 best_individual = None
 best_accuracy = 0.0
+best_fitness = 0.0
 
-start_time = time.time()
+total_time = time.time();
+# Ciclo fino a trovare un individuo con la fitness desiderata
+generation = 0
 
-# All'interno del ciclo delle generazioni
-for generation in range(generations):
+while best_fitness < target_fitness:
+    fitness_values = [fitness(individual, epochs) for individual in population]
 
-    fitness_values = [fitness(individual) for individual in population]
+    for fit in fitness_values:
+        accuracy, fitness_score, training_time, _, individual = fit  # Estrai i valori restituiti dalla fitness function
+        save_individual(population_filename, generation, individual, accuracy, fitness_score, training_time)
+    
+    accuracies = [fit[0] for fit in fitness_values]
+    fitness_scores = [fit[1] for fit in fitness_values]
+    training_times = [fit[2] for fit in fitness_values]
+    history = [fit[3] for fit in fitness_values];
+    
     # Identifica il miglior individuo nella popolazione corrente
-    best_index = np.argmax(fitness_values) if fitness_values else None
+    best_index = np.argmax(fitness_scores) if fitness_scores else None
 
     if best_index is not None:
         best_individual = population[best_index] # Selezione del miglior individuo
-        best_accuracy = fitness_values[best_index] # Selezione della miglior accuracy associata all'individuo
-        save_individuals(population_filename, best_individual, best_accuracy)
-        print(f'Generazione {generation + 1}: Miglior Accuracy = {best_accuracy}, Parametri = {best_individual}')
+        best_accuracy = accuracies[best_index] # Selezione dell' accuracy associata all'individuo
+        best_fitness = fitness_scores[best_index] # Selezione della fitness associata all'individuo
+        best_time = training_times[best_index] # Selezione del tempo di training del miglior individuo
+
+        print(f'Generazione {generation + 1}:  Miglior fitness = {best_fitness}, Accuracy = {best_accuracy} Parametri = {best_individual}')
     else:
         print(f'Generazione {generation + 1}: Nessun individuo valido nella popolazione')
 
     # Selezione, crossover e mutazione per generare la nuova popolazione
     new_population = []
-    for _ in range(population_size // 2):
-        parent1, parent2 = select_parents(population, fitness_values)
+    for _ in range(population_size // 2): # Si scelgono 10 genitori
+        parent1, parent2 = select_parents(population, fitness_scores)
         child1, child2 = crossover(parent1, parent2)
         child1 = mutate(child1)
         child2 = mutate(child2)
-        new_population.extend([child1, child2])
+        new_population.extend([child1, child2]) 
 
     population = new_population
 
-print(time.time() - start_time)
+    generation += 1
+
+print(f'Terminato dopo {generation} generazioni in {time.time()-total_time}. Individuo con la fitness desiderata: {best_individual}, Accuracy: {best_accuracy}, Fitness: {best_fitness}, addestrato in {best_time} secondi')
--- neural_network.py
+++ neural_network.py
@@ -6,11 +6,8 @@ from keras.datasets import cifar10
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0  # Normalizzazione dei pixel
 
-# Definizione della funzione per la creazione della CNN
+# Creazione della CNN
 def create_cnn(num_conv_layers, conv_size, num_fc_layers, fc_size):
-
-    if num_conv_layers <= 0 or num_fc_layers <= 0:
-        raise ValueError("Il numero di strati convoluzionali e fully-connected deve essere positivo.")
 
     model = models.Sequential()
 
@@ -18,7 +15,7 @@ def create_cnn(num_conv_layers, conv_size, num_fc_layers, fc_size):
     for _ in range(num_conv_layers):
         model.add(layers.Conv2D(conv_size, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
         model.add(layers.MaxPooling2D((2, 2)))
-
+        model.add(layers.BatchNormalization())
     
     model.add(layers.Flatten())
     model.add(Dropout(0.5)) # Dropout per limitare l'overfitting
@@ -26,23 +23,24 @@ def create_cnn(num_conv_layers, conv_size, num_fc_layers, fc_size):
     # Aggiunta degli strati fully-connected
     for _ in range(num_fc_layers):
         model.add(layers.Dense(fc_size, activation='relu'))
+        model.add(layers.BatchNormalization())
+        model.add(Dropout(0.3)) # Dropout per limitare l'overfitting
 
-    model.add(Dropout(0.3)) # Dropout per limitare l'overfitting
     model.add(layers.Dense(10, activation='softmax'))  # Strato di output
 
     return model
 
 
-# Definizione della funzione per l'addestramento della CNN
+# Addestramento della CNN
 def train_cnn(model, epochs):
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=epochs, validation_data=(x_test, y_test))
+    history = model.fit(x_train, y_train, epochs=epochs, validation_data=(x_test, y_test)) # Addestramento
 
-    _, accuracy = model.evaluate(x_test, y_test)
-    return accuracy
+    _, accuracy = model.evaluate(x_test, y_test) # Test
+    return accuracy, history
 
 
 
